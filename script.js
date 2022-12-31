5205c21caafc3a9125887cc0b94f060d0e14ef2a
Vishnu vardhan <vishnuvardhan18100@gmail.com>
2023-01-01 00:48:32 +0530

    initial commit
    initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+
+function jump(){
+    character.classList.add("animate");
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },500)
+}
+
+var checkdead = setInterval(function(){
+   var charactertop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+
+   var blockleft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+
+   if(blockleft<20 && blockleft>0 && charactertop>=130 ){
+
+    block.style.animation="none";
+    block.style.display="none";
+    alert("You Loose");
+ }
+   
+},10);
+
+
+    
+ 
+
+
+
+
